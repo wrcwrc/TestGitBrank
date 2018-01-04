2f60eae07caec04a0e0a585e0a633ab96f3db82b
韦荣炽 <weirongchi@weirongchideMacBook-Pro.local>
2018-01-04 20:05:52 +0800

    测试

---
+++ MyTestBrank/MyTestBrank/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTestBrank
+//
+//  Created by 韦荣炽 on 2018/1/4.
+//  Copyright © 2018年 XingFei_韦荣炽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTestBrank/MyTestBrank/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTestBrank
+//
+//  Created by 韦荣炽 on 2018/1/4.
+//  Copyright © 2018年 XingFei_韦荣炽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
